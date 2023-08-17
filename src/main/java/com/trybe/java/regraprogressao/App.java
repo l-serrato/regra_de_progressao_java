f19bd13dffa4af7ffe62c253c4315a27b69bffae
Luis <l-serrato@live.com>
2023-08-17 16:21:58 -0300

    done?

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,115 +1,45 @@
 package com.trybe.java.regraprogressao;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * App.
+ */
 public class App {
 
+  /**
+   * Metodo main.
+   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
-    ArrayList<Atividade> atividades = cadastrarAtividades(scanner);
-    ArrayList<Nota> notas = inserirNotas(scanner, atividades);
-    double percentualAlcancado = calcularResultado(atividades, notas);
-
-    if (percentualAlcancado >= 85) {
-      System.out.printf(
-          "Parabéns! Você alcançou %.2f%%! Temos o prazer de informar que você obteve aprovação!%n",
-          percentualAlcancado);
-    } else {
-      System.out.printf(
-          "Lamentamos informar que, com base na sua pontuação alcançada neste período, %.2f%%, você não atingiu a pontuação mínima necessária para sua aprovação.%n",
-          percentualAlcancado);
-    }
-
-    scanner.close();
-  }
-
-  public static ArrayList<Atividade> cadastrarAtividades(Scanner scanner) {
-    System.out.print("Digite a quantidade de atividades para cadastrar:\n");
-    int quantidadeAtividades = scanner.nextInt();
-    scanner.nextLine(); // Consumir a quebra de linha após o próximo inteiro
-
-    ArrayList<Atividade> atividades = new ArrayList<>();
-
-    for (int i = 0; i < quantidadeAtividades; i++) {
-      System.out.printf("Digite o nome da atividade %d:%n", i + 1);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int quantidadeAtividades = Integer.parseInt(scanner.nextLine());
+    int i = 0;
+    int[] somaNotas = new int[quantidadeAtividades];
+    int somaPesos = 0;
+    while (i < quantidadeAtividades) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
       String nome = scanner.nextLine();
-
-      System.out.printf("Digite o peso da atividade %d:%n", i + 1);
-      int peso = scanner.nextInt();
-      scanner.nextLine(); // Consumir a quebra de linha após o próximo inteiro
-
-      atividades.add(new Atividade(nome, peso));
+      System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
+      int peso = Integer.parseInt(scanner.nextLine());
+      System.out.println("Digite a nota obtida para " + nome + ": ");
+      int nota = Integer.parseInt(scanner.nextLine());
+      somaNotas[i] = peso * nota;
+      somaPesos += peso;
+      i++;
     }
-
-    return atividades;
-  }
-
-  public static ArrayList<Nota> inserirNotas(Scanner scanner, ArrayList<Atividade> atividades) {
-    ArrayList<Nota> notas = new ArrayList<>();
-
-    for (Atividade atividade : atividades) {
-      System.out.printf("Digite a nota obtida para %s:%n", atividade.getNome());
-      double nota = scanner.nextDouble();
-      scanner.nextLine(); // Consumir a quebra de linha após o próximo double
-
-      notas.add(new Nota(atividade, nota));
+    float somaNotasTotal = 0;
+    for (int j = 0; j < quantidadeAtividades; j++) {
+      somaNotasTotal += somaNotas[j];
     }
-
-    return notas;
-  }
-
-  public static double calcularResultado(ArrayList<Atividade> atividades, ArrayList<Nota> notas) {
-    double totalPontos = 0;
-    double totalPesos = 0;
-
-    for (int i = 0; i < atividades.size(); i++) {
-      Atividade atividade = atividades.get(i);
-      Nota nota = notas.get(i);
-
-      totalPontos += (nota.getPontos() / 100) * atividade.getPeso();
-      totalPesos += atividade.getPeso();
+    if (somaNotasTotal / somaPesos >= 85) {
+      System.out.println("Parabéns! Você alcançou " + somaNotasTotal / somaPesos
+          + "%! E temos o prazer de informar que você obteve aprovação! ");
+    } else {
+      System.out.println("Lamentamos informar que, "
+          + "com base na sua pontuação alcançada neste período, "
+          + somaNotasTotal / somaPesos + "%, você não atingiu a pontuação "
+          + "mínima necessária para sua aprovação.");
     }
-
-    return (totalPontos / totalPesos) * 100;
-  }
-}
-
-class Atividade {
-
-  private String nome;
-  private int peso;
-
-  public Atividade(String nome, int peso) {
-    this.nome = nome;
-    this.peso = peso;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public int getPeso() {
-    return peso;
-  }
-}
-
-class Nota {
-
-  private Atividade atividade;
-  private double pontos;
-
-  public Nota(Atividade atividade, double pontos) {
-    this.atividade = atividade;
-    this.pontos = pontos;
-  }
-
-  public Atividade getAtividade() {
-    return atividade;
-  }
-
-  public double getPontos() {
-    return pontos;
   }
 }

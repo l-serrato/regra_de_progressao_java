1c1c717e18c5daf43b63c4bb1c508fdbce3d9a56
Luis <l-serrato@live.com>
2023-08-17 16:26:33 -0300

    final

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -13,11 +13,11 @@ public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
-    int quantidadeAtividades = Integer.parseInt(scanner.nextLine());
+    int atividades = Integer.parseInt(scanner.nextLine());
     int i = 0;
-    int[] somaNotas = new int[quantidadeAtividades];
-    int somaPesos = 0;
-    while (i < quantidadeAtividades) {
+    int[] somaNotas = new int[atividades];
+    int pesos = 0;
+    while (i < atividades) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
       String nome = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
@@ -25,20 +25,20 @@ public class App {
       System.out.println("Digite a nota obtida para " + nome + ": ");
       int nota = Integer.parseInt(scanner.nextLine());
       somaNotas[i] = peso * nota;
-      somaPesos += peso;
+      pesos += peso;
       i++;
     }
-    float somaNotasTotal = 0;
-    for (int j = 0; j < quantidadeAtividades; j++) {
-      somaNotasTotal += somaNotas[j];
+    float notaTotal = 0;
+    for (int j = 0; j < atividades; j++) {
+      notaTotal += somaNotas[j];
     }
-    if (somaNotasTotal / somaPesos >= 85) {
-      System.out.println("Parabéns! Você alcançou " + somaNotasTotal / somaPesos
+    if (notaTotal / pesos >= 85) {
+      System.out.println("Parabéns! Você alcançou " + notaTotal / pesos
           + "%! E temos o prazer de informar que você obteve aprovação! ");
     } else {
       System.out.println("Lamentamos informar que, "
           + "com base na sua pontuação alcançada neste período, "
-          + somaNotasTotal / somaPesos + "%, você não atingiu a pontuação "
+          + notaTotal / pesos + "%, você não atingiu a pontuação "
           + "mínima necessária para sua aprovação.");
     }
   }
